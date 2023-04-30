49d865530ca7c3205fc93f810a78511f45c79346
tim910618 <s1410931050@gms.nutc.edu.tw>
2023-05-01 02:18:53 +0800

    TIM

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -276,20 +276,12 @@ public class HomeController : ControllerBase
                 updateData.GetType().GetProperty($"img{i + 1}").SetValue(updateData, newImgUrl, null);
             }
         }
-<<<<<<< Updated upstream
-        updateData.img1 = filenames.Count > 0 ? filenames[0].Replace("http://localhost:5190/Image/", "") : (updateData.img1 ?? data.img1);
-        updateData.img2 = filenames.Count > 1 ? filenames[1].Replace("http://localhost:5190/Image/", "") : (updateData.img2 ?? data.img2);
-        updateData.img3 = filenames.Count > 2 ? filenames[2].Replace("http://localhost:5190/Image/", "") : (updateData.img3 ?? data.img3);
-        updateData.img4 = filenames.Count > 3 ? filenames[3].Replace("http://localhost:5190/Image/", "") : (updateData.img4 ?? data.img4);
-        updateData.img5 = filenames.Count > 4 ? filenames[4].Replace("http://localhost:5190/Image/", "") : (updateData.img5 ?? data.img5);
-=======
 
         updateData.img1 = filenames.Count > 0 ? filenames[0].Replace("http://localhost:5190/Image/", "") : data.img1;
         updateData.img2 = filenames.Count > 1 ? filenames[1].Replace("http://localhost:5190/Image/", "") : data.img2;
         updateData.img3 = filenames.Count > 2 ? filenames[2].Replace("http://localhost:5190/Image/", "") : data.img3;
         updateData.img4 = filenames.Count > 3 ? filenames[3].Replace("http://localhost:5190/Image/", "") : data.img4;
         updateData.img5 = filenames.Count > 4 ? filenames[4].Replace("http://localhost:5190/Image/", "") : data.img5;
->>>>>>> Stashed changes
 
         /*updateData.img1 = filenames.Count > 0 ? filenames[0] : data.img1;
         updateData.img2 = filenames.Count > 1 ? filenames[1] : data.img2;
